d5e3b855a144dd1ad37271113bdd42fc92c8dc48
sdgh <chipsekachap@gmail.com>
2026-08-26 22:30:55 +0330

    fix

--- lib/DateConverter/DateConverter.h
+++ lib/DateConverter/DateConverter.h
@@ -12,7 +12,7 @@ typedef struct {
 Date_t gregorianToPersian_ChatGPT(const struct tm& date);
 bool isPersianLeapYear_ChatGPT(int jy);
 
-Date_t gregorianToPersian_Claude(const struct tm& date);
+Date_t gregorianToPersian_Claude(int gy, int gm, int gd);
 bool isPersianLeapYear_Claude(int jy);
 
 
